e44624e1a59ecbb462a1a873fda7bca05f9bf75f
Peter Mowen <pmmowen@gmail.com>
2020-03-22 00:00:32 -0400

    Starts refactor. Adds Thermometer.cpp. Guts .ino
    The original file, NetworkedThermostat.ino has become unreadable so a
    refactor is in order. This commit starts that process. The goal of the
    refactor is to make the code more modular. Since the room nodes will
    share a large code base with the thermostat, it is prudent to refactor
    the code in such a way as to make that easy. At the end of this
    refactor, there will be two main objects: Thermometer and Thermostat.
    
    This commits starts the Thermometer object. Thermometer.ccp is a class
    file. This commit adds the ability to create a Thermometer object,
    connect the esp8266 to the si7021 chip via I2C, and read the ambient
    temperature. NetworkedThermostat.ino is updated to create a
    Thermometer object, read the ambient temperature, and print it to
    Serial.

---
+++ Thermometer.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include <Wire.h>
+class Thermometer
+{
+    float temperature;  // current temperature
+    int sda;            // SDA Pin
+    int scl;            // SCL Pin
+    int addr = 0x40;   // I2C address of si7021
+    
+public:
+    
+    Thermometer(int SDA, int SCL)
+    {
+        sda = SDA;
+        scl = SCL;
+    }
+
+    void setup()
+    {
+        Wire.begin(sda, scl);
+        Wire.beginTransmission(addr);
+        Wire.write(0xFE); // Write reset command
+        Wire.endTransmission();
+        delay(20);
+    }
+
+    void loop()
+    {
+        readAmbientTemperature();
+    }
+    
+    float readAmbientTemperature()
+    {    
+        unsigned int measurement;
+        Wire.beginTransmission(addr);
+        // Send temperature measurement code
+        Wire.write(0xF3);
+        Wire.endTransmission();
+        delay(20);
+    
+        // Request 2 bytes of data
+        Wire.requestFrom(addr, 2);
+    
+        //Read 2 bytes of data for temperature
+        if (Wire.available() == 2)
+        {
+            unsigned int msb = Wire.read();
+            unsigned int lsb = Wire.read();
+            // Clear the last two bits of LSB to 00.
+            // According to datasheet LSB of RH is always xxxxxx10
+            lsb &= 0xFC;
+            measurement = msb << 8 | lsb;
+        }
+    
+        // Convert the data
+        temperature = ((175.72 * (float)measurement) / 65536.0) - 46.85;
+    }
+};
